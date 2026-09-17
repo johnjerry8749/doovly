a3ee66e8abd4eb39bd2c250da8937d76e701bd03
John <johnjerry8749@gmail.com>
2026-09-17 21:00:25 +0100

    save

--- src/app/(tab)/home.tsx
+++ src/app/(tab)/home.tsx
@@ -318,7 +318,7 @@ export default function Home() {
           </TouchableOpacity>
         </View>
 
-        /* ================= PROFESSIONALS ================= */}
+       { /* ================= PROFESSIONALS ================= */}
 
         <ScrollView
           horizontal
