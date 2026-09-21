dda1a1cec2e0b7f0c0928230b500eac757a100f0
John <johnjerry8749@gmail.com>
2026-09-21 12:40:48 +0100

    savee

--- src/app/(tab)/bookings.tsx
+++ src/app/(tab)/bookings.tsx
@@ -913,7 +913,7 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
   },
   modalBackdrop: {
-    ...StyleSheet.absoluteFillObject,
+    ...StyleSheet.absoluteFill,
     backgroundColor: "rgba(0,0,0,0.45)",
   },
   modalSheet: {
