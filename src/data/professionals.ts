1830ea81e86661cc394a4dc6b4d1e8f570952582
John <johnjerry8749@gmail.com>
2026-09-23 22:27:38 +0100

    save

--- src/data/professionals.ts
+++ src/data/professionals.ts
@@ -80,7 +80,7 @@ export const PROFESSIONALS: Professional[] = [
     priceFrom: "₦8,000",
     image: require("@/assets/profile_1.jpg"),
     verified: false,
-    subscribed: true,
+    subscribed: false,
     latitude: 6.5244,
     longitude: 3.3792,
     role: "admin",
