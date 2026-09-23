19555c26b0f336f8451578867af66b1af50897fe
John <johnjerry8749@gmail.com>
2026-09-23 23:28:46 +0100

    save

--- src/app/(tab)/_layout.tsx
+++ src/app/(tab)/_layout.tsx
@@ -43,6 +43,16 @@ export default function TabLayout() {
       />
 
       <Tabs.Screen
+        name="requests"
+        options={{
+          title: "Requests",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="briefcase-outline" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
         name="bookings"
         options={{
           title: "Bookings",
