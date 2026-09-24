5533e53171924df2b924c3bb9267c3e695a5b6c5
John <johnjerry8749@gmail.com>
2026-09-24 22:56:30 +0100

    remove count badge on service request

--- src/app/(tab)/profile.tsx
+++ src/app/(tab)/profile.tsx
@@ -271,11 +271,6 @@ export default function Profile() {
                 <MenuIcon name="list-outline" />
               }
               title="Service Requests"
-              badge={
-                requestCount > 0
-                  ? requestCount
-                  : undefined
-              }
               onPress={() =>
                 router.push(
                   "/profile/my_service_requests",
